3523352501516e191497c588899043aed2cba1ca
Amir Maso'ud Mehrabian <aut0run2011@gmail.com>
2021-04-30 15:02:20 +0430

    fixed some issues

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,10 +7,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
+    protected $appends = array('url');
     use HasFactory;
 
     public function author()
     {
         return $this->belongsTo('App\Models\User');
+    }
+
+    public function getUrlAttribute()
+    {
+        return route('api.v1.posts.show', $this->id);
     }
 }
